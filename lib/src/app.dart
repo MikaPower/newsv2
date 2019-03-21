dbdba74805548049db712a8dc62849ed62fdc020
mikapower <35053@ufp.edu.pt>
2019-03-21 19:27:02 +0000

    updates

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
 
 import 'screens/news_list.dart';
+import 'blocs/stories_provider.dart';
 
 class App extends StatelessWidget{
   Widget build(BuildContext context) {
-    return MaterialApp(
+   return new StoriesProvider(
+      child:  new MaterialApp(
       title: 'News',
       home: NewsList(),
+    ),
     );
+
   }
 
 }
--- lib/src/screens/news_list.dart
+++ lib/src/screens/news_list.dart
@@ -1,12 +1,39 @@
 import 'package:flutter/material.dart';
+import 'package:news/src/blocs/stories_bloc.dart';
+import 'dart:async';
+import '../blocs/stories_provider.dart';
 
-class NewsList extends StatelessWidget{
-  Widget build (context){
+class NewsList extends StatelessWidget {
+  Widget build(context) {
+    final bloc = StoriesProvider.of(context);
+    //this is bad
+    bloc.fetchTopIds();
     return new Scaffold(
       appBar: new AppBar(
-        title: new Text("Top news"),
+        title: new Text("TopbuildList"),
       ),
-      body: new Text('Show some news here!')
+      body: buildList(bloc),
+    );
+  }
+
+  Widget buildList(StoriesBloc bloc) {
+    return StreamBuilder(
+        stream: bloc.topIds,
+        builder: (context, AsyncSnapshot<List<int>>snapshot) {
+          if (!snapshot.hasData) {
+            return new Center(
+              child: new CircularProgressIndicator(),
+            );
+
+          }
+          return new ListView.builder(
+            itemCount: snapshot.data.length,
+            itemBuilder: (context, int index) {
+              return new Text('${snapshot.data[index]}');
+            },
+          );
+        }
     );
   }
 }
+
